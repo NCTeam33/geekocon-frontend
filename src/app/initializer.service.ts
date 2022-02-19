4ddc91828a49ab023a4eda71197f1f25bd3bf183
V3ss3l <grishav3ss3l@gmail.com>
2022-02-19 19:41:37 +0400

    Keycloak try

---
+++ src/app/initializer.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { environment as env} from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InitializerService {
+
+  constructor() { }
+}
+
+export function initializer(keycloak: KeycloakService): () => Promise<any> {
+  return (): Promise<any> => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await keycloak.init({
+          config: {
+            url: env.keycloak.issuer,
+            realm: env.keycloak.realm,
+            clientId: env.keycloak.clientId,
+          },
+          loadUserProfileAtStartUp: true,
+          initOptions: {
+            onLoad: 'login-required'
+          },
+          bearerExcludedUrls: []
+        });
+        Promise.resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+}
---
+++ src/app/user-nav/user-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-nav',
+  templateUrl: './user-nav.component.html',
+  styleUrls: ['./user-nav.component.less']
+})
+export class UserNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
