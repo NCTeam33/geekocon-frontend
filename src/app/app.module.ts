a28b71ac6e048c8c0808eeee1e34ecd9ea3bd2b4
V3ss3l <grishav3ss3l@gmail.com>
2022-04-07 22:58:46 +0400

    Little Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminNavComponent } from './admin-nav/admin-nav.component';
 import { UserNavComponent } from './user-nav/user-nav.component';
 import { initializer } from './initializer.service';
 import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
@@ -13,7 +12,6 @@ import { ContributorsComponent } from './contributors/contributors.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AdminNavComponent,
     UserNavComponent,
     ZonesComponent,
     ContributorsComponent,
